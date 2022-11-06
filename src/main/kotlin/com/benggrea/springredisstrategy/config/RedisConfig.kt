55144b6b6977267d187a40a415e6aa066ed28b84
benggrae <kbh0581@gmail.com>
2022-11-06 14:30:22 +0900

    env : redis docker-compose 셋팅 및 레디스 셋팅

--- src/main/kotlin/com/benggrea/springredisstrategy/config/RedisConfig.kt
+++ src/main/kotlin/com/benggrea/springredisstrategy/config/RedisConfig.kt
@@ -2,12 +2,16 @@ package com.benggrea.springredisstrategy.config
 
 import com.benggrea.springredisstrategy.domain.Data
 import com.benggrea.springredisstrategy.infra.DataJpaRepository
+import com.fasterxml.jackson.databind.ser.std.StdKeySerializers.StringKeySerializer
+import com.fasterxml.jackson.databind.ser.std.StringSerializer
 import org.springframework.boot.autoconfigure.data.redis.RedisProperties
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.data.redis.connection.RedisConnectionFactory
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory
 import org.springframework.data.redis.core.RedisTemplate
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer
+import org.springframework.data.redis.serializer.StringRedisSerializer
 import javax.annotation.PostConstruct
 
 @Configuration
@@ -22,19 +26,24 @@ class RedisConfig(val redisProperties: RedisProperties) {
     fun redisTemplate(): RedisTemplate<Any, Any> {
         val redisTemplate = RedisTemplate<Any, Any>()
         redisTemplate.setConnectionFactory(lettuceConnectionFactory())
+        redisTemplate.keySerializer = StringRedisSerializer()
+        redisTemplate.valueSerializer = GenericJackson2JsonRedisSerializer()
         return redisTemplate
     }
 }
 
+
 @Configuration
 class SampleDataWarmUp(
-    val dataJpaRepository: DataJpaRepository
+    val dataJpaRepository: DataJpaRepository,
+    val redisTemplate: RedisTemplate<Any, Any>
     ) {
 
     @PostConstruct
     fun setUp() {
         val datas = listOf(Data("a"), Data("b"), Data("c"))
         dataJpaRepository.saveAll(datas)
+        redisTemplate.opsForSet().add("data", datas)
     }
 
 }
--- src/main/kotlin/com/benggrea/springredisstrategy/infra/CacheAsideCommand.kt
+++
@@ -1,9 +0,0 @@
-package com.benggrea.springredisstrategy.infra
-
-import org.springframework.stereotype.Service
-
-@Service
-class CacheAsideCommand() {
-
-
-}
